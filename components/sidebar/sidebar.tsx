8894a390e83d92fdbf4e0c8b356c666707a35487
Jobayer Ahammed <nesarahammed019@gmail.com>
2023-10-30 20:00:56 +0600

    Server page sidebar done

---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,70 @@
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import SidebarAction from "@/components/sidebar/sidebar-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import SidebarItem from "./sidebar-item";
+import { ModeToggle } from "../mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+const Sidebar = async () => {
+
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  });
+
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full w-full text-primary py-3 dark:bg-[#1E1F22]">
+
+      {/* Create Server Button */}
+      <SidebarAction />
+      <Separator 
+        className="h-[2px] w-10 mx-auto bg-zinc-300 dark:bg-zinc-700 rounded-md"
+      />
+
+      {/* Scroll Area */}
+      <ScrollArea className="flex-1 w-full">
+        {
+          servers.map(server => (
+            <div className="mb-4" key={server.id}>
+              <SidebarItem
+                id={server.id}
+                name={server.name}
+                imgUrl={server.imgUrl}
+              />
+            </div>
+          ))
+        }
+      </ScrollArea>
+
+      {/* User button */}
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[45px] w-[45px]"
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+ 
+export default Sidebar;
